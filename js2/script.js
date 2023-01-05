fcd8972bb9d77cc8652237142da4527e180a0739
cyril hamel <xizor64@gmail.com>
2023-01-05 12:18:52 +0100

    maj_05_01_23_midi

--- js2/script.js
+++ js2/script.js
@@ -1,71 +1,76 @@
 //navBar canvas
-let canvas=document.getElementById("c1");
-let ctx=canvas.getContext("2d");
+function maRedirection(lien) {
+    location.href = lien;
+}
 
-canvas.width=355;
-canvas.height=355;
 
-ctx.fillStyle="#D75414";
-ctx.fillRect(0,0,355,355);
+let canvas = document.getElementById("c1");
+let ctx = canvas.getContext("2d");
+
+canvas.width = 355;
+canvas.height = 355;
+
+ctx.fillStyle = "#D75414";
+ctx.fillRect(0, 0, 355, 355);
 
 
 
 //cuisinier Traiteur
 ctx.beginPath();
-ctx.lineWidth="5";
-ctx.fillStyle="#E0AC21";
-ctx.strokeStyle="#E0AC21";
-ctx.arc(50,50,300,0,2*Math.PI);
+ctx.lineWidth = "5";
+ctx.fillStyle = "#E0AC21";
+ctx.strokeStyle = "#E0AC21";
+ctx.arc(50, 50, 300, 0, 2 * Math.PI);
 ctx.closePath();
 ctx.stroke();
 ctx.fill();
 
 //Patisserie
 ctx.beginPath();
-ctx.lineWidth="5";
-ctx.fillStyle="#C02D0C";
-ctx.strokeStyle="#C02D0C";
-ctx.arc(50,50,260,0,2*Math.PI);
+ctx.lineWidth = "5";
+ctx.fillStyle = "#C02D0C";
+ctx.strokeStyle = "#C02D0C";
+ctx.arc(50, 50, 260, 0, 2 * Math.PI);
 ctx.closePath();
 ctx.stroke();
 ctx.fill();
 
 //Produits
 ctx.beginPath();
-ctx.lineWidth="5";
-ctx.fillStyle="#D75414";
-ctx.strokeStyle="#D75414";
-ctx.arc(50,50,220,0,2*Math.PI);
+ctx.lineWidth = "5";
+ctx.fillStyle = "#D75414";
+ctx.strokeStyle = "#D75414";
+ctx.arc(50, 50, 220, 0, 2 * Math.PI);
 ctx.closePath();
 ctx.stroke();
 ctx.fill();
 
 //commandes
 ctx.beginPath();
-ctx.lineWidth="5";
-ctx.fillStyle="#BF2C0C";
-ctx.strokeStyle="#BF2C0C";
-ctx.arc(50,50,180,0,2*Math.PI);
+ctx.lineWidth = "5";
+ctx.fillStyle = "#BF2C0C";
+ctx.strokeStyle = "#BF2C0C";
+ctx.arc(50, 50, 180, 0, 2 * Math.PI);
 ctx.closePath();
 ctx.stroke();
 ctx.fill();
 
 //formations
 ctx.beginPath();
-ctx.lineWidth="5";
-ctx.fillStyle="#A80000";
-ctx.strokeStyle="#A80000";
-ctx.arc(50,50,140,0,2*Math.PI);
+ctx.lineWidth = "5";
+ctx.fillStyle = "#A80000";
+ctx.strokeStyle = "#A80000";
+ctx.arc(50, 50, 140, 0, 2 * Math.PI);
 ctx.closePath();
 ctx.stroke();
 ctx.fill();
 
 //accueil
 ctx.beginPath();
-ctx.lineWidth="5";
-ctx.fillStyle="#61130A";
-ctx.strokeStyle="#61130A";
-ctx.arc(50,50,100,0,2*Math.PI);
+ctx.lineWidth = "5";
+ctx.fillStyle = "#61130A";
+ctx.strokeStyle = "#61130A";
+ctx.arc(50, 50, 100, 0, 2 * Math.PI);
 ctx.closePath();
 ctx.stroke();
 ctx.fill();
